49a7ec03f6a11a6827e8d4cc1e7b8dc9e2f2bf4d
Henrik Drefs <henrik.drefs@outlook.de>
2020-02-10 01:15:37 +0100

    removed comments

--- functions/jira-proxy.js
+++ functions/jira-proxy.js
@@ -1,49 +1,5 @@
 const fetch = require('node-fetch');
 
-// app.get('/*', async (req, res) => {
-//   const serverUrl = req.header('X-JIRA-Server-Url');
-//   if (!serverUrl) {
-//     res.status(400).json({ error: 'X-JIRA-Server-Url header missing' });
-//     return;
-//   }
-//   const cookie = req.header('X-JIRA-Cookie');
-//   try {
-//     const jiraResponse = await fetch(serverUrl + req.url, {
-//       headers: {
-//         cookie,
-//       },
-//     });
-//     if (!jiraResponse.ok) {
-//       res.status(jiraResponse.status).send();
-//       return;
-//     }
-//     const data = await jiraResponse.json();
-//     res.json(data);
-//   } catch {
-//     res.status(400).json({ error: 'Error during request' });
-//   }
-// });
-
-// app.post('/*', async (req, res) => {
-//   const serverUrl = req.header('X-JIRA-Server-Url');
-//   if (!serverUrl) {
-//     res.status(400).json({ error: 'X-JIRA-Server-Url header missing' });
-//     return;
-//   }
-//   try {
-//     const jiraResponse = await fetch(serverUrl + req.url, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(req.body),
-//     });
-//     const data = await jiraResponse.json();
-//     res.json(data);
-//   } catch {
-//     res.status(400).json({ error: 'Error during request' });
-//   }
-// });
 function stringifyQueryParams(queryParams) {
   if (!queryParams || Object.keys(queryParams).length === 0) {
     return '';
