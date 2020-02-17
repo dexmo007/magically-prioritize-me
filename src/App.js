4fa30e6d4e37cbd630b9185453c49320f04aa614
Henrik Drefs <henrik.drefs@outlook.de>
2020-02-17 21:25:10 +0100

    logo design, router, landing page

--- src/App.js
+++ src/App.js
@@ -1,193 +1,56 @@
 import React from 'react';
-import './App.css';
-import '@atlaskit/css-reset';
-import { ProgressTracker } from '@atlaskit/progress-tracker';
-import Page, { Grid, GridColumn } from '@atlaskit/page';
-import PageHeader from '@atlaskit/page-header';
-import ConnectStep from './components/setup/ConnectStep';
-import SelectProjectStep from './components/setup/SelectProjectStep';
-import SelectBoardStep from './components/setup/SelectBoardStep';
-import AppContext, { initContext } from './AppContext';
-import ReLoginModal from './components/setup/ReLoginModal';
-import SelectSprintStep from './components/setup/SelectSprintStep';
-import SetupTeamStep from './components/setup/SetupTeamStep';
-import MagicPrioritizationWrapper from './components/MagicPrioritizationWrapper';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { LayoutManager, NavigationProvider } from '@atlaskit/navigation-next';
+import Home from './Home';
+import GlobalNavigation from '@atlaskit/global-navigation';
+import JiraServerApp from './JiraServerApp';
+import { ReactComponent as Logo } from './assets/logo.svg';
+import Icon from '@atlaskit/icon';
 
-const initialStages = [
-  {
-    id: 'connect',
-    label: 'Connect to JIRA',
-    percentageComplete: 0,
-    status: 'current',
-    noLink: true,
-    component: ConnectStep,
-  },
-  {
-    id: 'select-project',
-    label: 'Select a project',
-    percentageComplete: 0,
-    status: 'unvisited',
-    noLink: true,
-    component: SelectProjectStep,
-  },
-  {
-    id: 'select-board',
-    label: 'Select a board',
-    percentageComplete: 0,
-    status: 'unvisited',
-    noLink: true,
-    component: SelectBoardStep,
-  },
-  {
-    id: 'select-sprint',
-    label: 'Select a sprint',
-    percentageComplete: 0,
-    status: 'unvisited',
-    noLink: true,
-    component: SelectSprintStep,
-  },
-  {
-    id: 'setup-team',
-    label: 'Setup team',
-    percentageComplete: 0,
-    status: 'unvisited',
-    noLink: true,
-    component: SetupTeamStep,
-  },
-];
-
-function initStages(ctx) {
-  if (!ctx.stage) {
-    return initialStages;
-  }
-  if (ctx.stage === 'DONE') {
-    return initialStages.map((stage) => ({
-      ...stage,
-      percentageComplete: 100,
-      status: 'visited',
-    }));
-  }
-  const idx = initialStages.findIndex(({ id }) => id === ctx.stage);
-  return [
-    ...initialStages.slice(0, idx).map((stage) => ({
-      ...stage,
-      percentageComplete: 100,
-      status: 'visited',
-    })),
-    { ...initialStages[idx], status: 'current' },
-    ...initialStages.slice(idx + 1),
-  ];
+function LogoIcon(props) {
+  return <Icon glyph={Logo} {...props} />;
 }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    const ctx = initContext();
-    console.log('initial context', ctx);
-
-    this.state = {
-      ctx,
-      stages: initStages(ctx),
-    };
-  }
-
-  advance = (data) => {
-    const next = this.state.stages.findIndex(
-      ({ status }) => status === 'unvisited'
-    );
-    if (next === -1) {
-      this.setState(
-        ({ stages, ctx }) => ({
-          ctx: { ...ctx, ...data },
-          stages: [
-            ...stages.slice(0, stages.length - 1),
-            {
-              ...stages[stages.length - 1],
-              percentageComplete: 100,
-              status: 'visited',
-            },
-          ],
-        }),
-        () => {
-          localStorage.setItem(
-            'ctx',
-            JSON.stringify({ ...this.state.ctx, stage: 'DONE' })
-          );
-        }
-      );
-      return;
-    }
-    this.setState(
-      ({ stages, ctx }) => ({
-        ctx: { ...ctx, ...data },
-        stages: [
-          ...stages.slice(0, next - 1),
-          { ...stages[next - 1], percentageComplete: 100, status: 'visited' },
-          { ...stages[next], status: 'current' },
-          ...stages.slice(next + 1),
-        ],
-      }),
-      () => {
-        localStorage.setItem(
-          'ctx',
-          JSON.stringify({ ...this.state.ctx, stage: this.currentStage().id })
-        );
-      }
-    );
-  };
-
-  currentStage = () =>
-    this.state.stages.find(({ status }) => status === 'current');
-
-  render() {
-    return (
-      <AppContext.Provider
-        value={{
-          data: this.state.ctx,
-          update: (newCtx) => {
-            this.setState({ ctx: newCtx });
-            localStorage.setItem(
-              'ctx',
-              JSON.stringify({
-                ...this.state.ctx,
-                stage: this.currentStage() ? this.currentStage().id : 'DONE',
-              })
-            );
-          },
-        }}
-      >
-        <Page>
-          <Grid>
-            <PageHeader>Magic Prioritization</PageHeader>
-          </Grid>
-          {this.currentStage() ? (
-            this.renderSetup()
-          ) : (
-            <MagicPrioritizationWrapper />
-          )}
-        </Page>
-        <ReLoginModal
-          isOpen={this.state.ctx.session === 'EXPIRED'}
-        ></ReLoginModal>
-      </AppContext.Provider>
-    );
-  }
-
-  renderSetup = () => {
-    const { component: Step } = this.currentStage();
-    return (
-      <React.Fragment>
-        <ProgressTracker items={this.state.stages} />
-        <Grid>
-          <GridColumn>
-            <div style={{ marginTop: '2em' }}>
-              <Step onFinish={this.advance}></Step>
-            </div>
-          </GridColumn>
-        </Grid>
-      </React.Fragment>
-    );
-  };
+const Global = () => (
+  <GlobalNavigation
+    productIcon={LogoIcon}
+    productLabel="Magic Prioritization"
+    productTooltip="Magic Prioritization"
+    productHref="/"
+    onProductClick={() => console.log('product clicked')}
+    onHelpClick={() => console.log('help clicked')}
+    helpItems={() => <div />}
+  />
+);
+function App() {
+  return (
+    <Router>
+      <NavigationProvider>
+        <LayoutManager
+          globalNavigation={Global}
+          productNavigation={() => null}
+          containerNavigation={() => null}
+          showContextualNavigation={false}
+          shouldHideGlobalNavShadow
+        >
+          <Switch>
+            <Route path="/jira-cloud">
+              <div>JIRA Cloud</div>
+            </Route>
+            <Route path="/jira-server">
+              <JiraServerApp />
+            </Route>
+            <Route path="/file">
+              <div>File </div>
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </LayoutManager>
+      </NavigationProvider>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,163 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ReactComponent as CloudIcon } from './assets/admin-cloud-tab-icon.svg';
+import { ReactComponent as ServerIcon } from './assets/admin-server-tab-icon.svg';
+import { ReactComponent as FileIcon } from './assets/file.svg';
+import EmojiSymbolsIcon from '@atlaskit/icon/glyph/emoji/symbols';
+import { ReactComponent as Logo } from './assets/logo.svg';
+
+const Wrapper = styled.div`
+  width: calc(100%+24px);
+  height: 100%;
+  padding: 16px;
+  margin-left: -24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: wrap;
+  background-color: #0747a6;
+`;
+
+const SpotlightLink = styled(Link)`
+  background-color: white;
+  margin: 1em;
+  width: 300px;
+  height: 300px;
+  border-radius: 3px;
+  text-decoration: none;
+  color: inherit;
+  transition: transform 0.3s cubic-bezier(0.15, 1, 0.33, 1);
+  &:hover {
+    color: inherit;
+    text-decoration: none;
+    transform: translateY(-2px);
+  }
+`;
+
+function SpotlightCard({ children, image, title, to }) {
+  return (
+    <SpotlightLink to={to}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: '18px',
+          fontWeight: 450,
+          padding: '1em',
+        }}
+      >
+        {title}
+      </div>
+      <div
+        style={{
+          padding: '1em',
+        }}
+      >
+        {children}
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>{image}</div>
+    </SpotlightLink>
+  );
+}
+export default function Home() {
+  return (
+    <Wrapper>
+      <header
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginBottom: '80px',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            color: 'white',
+            fontSize: '52px',
+            marginTop: '80px',
+          }}
+        >
+          <Logo height="80px" width="80px" style={{ margin: '18px' }} />
+          <h1 style={{ color: 'white', fontSize: '52px', margin: 0 }}>
+            Magic Prioritization
+          </h1>
+        </div>
+        <span style={{ color: 'white', fontSize: '24px', marginTop: '24px' }}>
+          Apply Magic Prioritization to your JIRA issues
+        </span>
+      </header>
+      <main
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          flex: 1,
+        }}
+      >
+        <div style={{ color: 'white' }}>Choose a source for your issues</div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <SpotlightCard
+            image={
+              <CloudIcon
+                style={{
+                  width: 36.8 * 4 + 'px',
+                  height: 36 * 4 + 'px',
+                }}
+              />
+            }
+            title="JIRA Cloud"
+            to="/jira-cloud"
+          >
+            Issues can be imported from a JIRA Cloud site.
+          </SpotlightCard>
+          <SpotlightCard
+            image={
+              <ServerIcon
+                style={{
+                  width: 36.8 * 4 + 'px',
+                  height: 36 * 4 + 'px',
+                }}
+              />
+            }
+            title="JIRA Server"
+            to="/jira-server"
+          >
+            Issues can be imported from your own JIRA server.
+          </SpotlightCard>
+          <SpotlightCard
+            image={
+              <FileIcon
+                width={36 * 4 + 'px'}
+                height={36 * 4 + 'px'}
+                fill="#0747a6"
+              />
+            }
+            title="File"
+            to="/file"
+          >
+            You can import issues from a JIRA export file (CSV, XML, XLSX).
+          </SpotlightCard>
+        </div>
+      </main>
+      <footer style={{ color: 'white' }}>
+        <span>Made with </span>
+        <EmojiSymbolsIcon size="small" />
+        <span> by </span>
+        <a
+          href="https://github.com/dexmo007"
+          style={{
+            color: 'white',
+            textDecoration: 'underline',
+          }}
+        >
+          dexmo
+        </a>
+      </footer>
+    </Wrapper>
+  );
+}
